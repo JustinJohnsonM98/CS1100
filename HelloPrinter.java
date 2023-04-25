fc8b972c24892eac5ba51d9807568bc62b96f0f8
JustinJohnsonM98 <114102724+JustinJohnsonM98@users.noreply.github.com>
2023-04-24 21:46:16 -0400

    Add files via upload

---
+++ HelloPrinter.java
@@ -0,0 +1,33 @@
+//Justin Johnson
+//ZKG641
+import java.util.Scanner;
+ public class HelloPrinter
+{
+	 public static void main(String[] args) {//begin main 
+		 //the following line of code prints a message to the screen 
+		 System.out.println("Hello Friends!");
+		 System.out.println("                 ");
+		 System.out.println(1+2+3+4+5+6+7+8+9+10);
+		 System.out.println("                     ");
+		 System.out.println(1*2*3*4*5*6*7*8*9*10);
+		 
+			 System.out.println();
+		 System.out.println(" * ");
+		 System.out.println("***");
+		System.out.println(" ******");
+	  System.out.println("*********");
+		System.out.println("******");
+		 System.out.println("***");
+		  System.out.println("*");
+		 System.out.println("Sales Tax Rates");
+		 System.out.println(" **        ****");
+		 System.out.println("South Carolina  6%");
+		 System.out.println("Idaho           6%");
+		 System.out.println("Texas           6.25%");
+		 System.out.println("Noth Carolina   4.75%");
+		 System.out.println("Mississipi      7%");
+		 String name=javax.swing.JOptionPane.showInputDialog("What is your name?");
+		 System.out.println("hello " + name);
+		 
+	 }// end main
+}
